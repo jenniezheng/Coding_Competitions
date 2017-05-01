df75be99751d4b8d8d59376bbe2535a536313881
Jennie Zheng <jenniezheng321@gmail.com>
2017-04-30 17:12:08 -0700

    square

--- python/squareLaminae.py
+++ python/squareLaminae.py
@@ -1,1 +1,10 @@
+import math
 #Problem 173 from projecteuler.net
+tilesAvailable=int(input())
+myMax=int(math.sqrt(tilesAvailable))
+sum=0
+for k in range (2,myMax, 2):
+	add=(tilesAvailable//k-k)//2
+	if(add>0):
+		sum+=add
+print(sum)
